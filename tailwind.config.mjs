28ed3b6321144ee5460dbea2a9d0bae5ea69203e
Aaron Palacios Olea <aaron.palacios@viewnext.com>
2024-07-09 15:40:31 +0200

    tailwind integration

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
+	theme: {
+		extend: {
+			colors: {
+				'regal-blue': '#0B76A6'
+			}
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	]
+}
